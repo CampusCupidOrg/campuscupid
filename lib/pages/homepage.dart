3d6ad33f40faeed0254bc2bf37a9a589107f7c1a
Prasanjit-4 <prasanjitpanda8@gmail.com>
2024-02-10 01:42:00 +0530

    home page basic ui

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:random_avatar/random_avatar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,7 +9,46 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Campus Cupid'),
+        elevation: 0.0,
+        title: Text(
+          'Campus Cupid',
+          style: GoogleFonts.pacifico(
+              fontSize: 30, color: Color.fromARGB(221, 58, 58, 58)),
+        ),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.list_alt_outlined),
+            label: 'Crush List',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(
+              label: Text('2'),
+              child: Icon(Icons.messenger_sharp),
+            ),
+            label: 'Messages',
+          ),
+        ],
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
       ),
     );
   }
