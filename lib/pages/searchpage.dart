bb31c05e1b0c5689e50370bde57036819cbb0846
Prasanjit-4 <prasanjitpanda8@gmail.com>
2024-02-19 21:51:05 +0530

    small changes

--- lib/pages/searchpage.dart
+++ lib/pages/searchpage.dart
@@ -22,11 +22,10 @@ class _SearchState extends State<Search> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: TextFormField(
-                style: Theme.of(context).textTheme.bodyText1,
                 onChanged: _onSearchFieldChanged,
                 autocorrect: false,
                 autofocus: true,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: "Name",
                   border: InputBorder.none,
                   focusedBorder: InputBorder.none,
@@ -73,47 +72,16 @@ class _SearchState extends State<Search> {
     });
   }
 
-  /// Searches our user database via the supabase_flutter package.
-  ///
-  /// Returns a list of user names.
-  ///
-  /// WARNING:
-  /// - in a more realistic example, this would be moved to a "repository" instead
-  ///   optionally with something like a FutureBuilder
-  /// - check fluttercrashcourse.com for tutorials on these concepts
   Future<List<String>> _searchUsers(String name) async {
-    // here, we leverage Supabase's (Postgres') full text search feature
-    // for super fast text searching without the need for something overkill for
-    // an example like this such as ElasticSearch or Algolia
-    //
-    // more info on Supabase's full text search here
-    // https://supabase.com/docs/guides/database/full-text-search
-
-    // WARNING: we aren't doing proper error handling here,
-    // as this is an example but typically we'd handle any exceptions via the
-    // callee of this function
-    // NOTE: this seaches our 'fts' (full text search column)
-    // NOTE: 'limit()' will improve the performance of the call as well.
-    // normally, we'd use a proper backend search index that would provide
-    // us with the most relevant results, vs simply using a wildcard match
     final result = await Supabase.instance.client
         .from('profiles')
         .select('full_name')
         .textSearch('fts', "$name:*");
 
-    // WARNING: we aren't doing proper response error code handling here.
-    // normally, we're present some kind of feedback to the user if this fails
-    // and optionally report it to an external tracking system such as Sentry,
-    // Rollbar, etc
-
     final List<String> names = [];
     for (var row in result) {
       names.add(row['full_name'] as String);
     }
-
-    // convert results into a list here
-    // 'result.data' is a list of Maps, where each map represents a returned
-    // row in our database. each key of the map represents a table column
 
     return names;
   }
