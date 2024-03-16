acc98614de3bd14bf11766a3517dd45b8cdabc83
vinay-04 <vinayrajan04@gmail.com>
2024-03-16 17:34:49 +0530

    fix: UI

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
 import 'package:campuscupid/main.dart';
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
+=======
+import 'package:flutter/widgets.dart';
+>>>>>>> e8fc3d7 (fix: UI)
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -75,7 +79,11 @@ class _LoginPageState extends State<LoginPage> {
       final session = data.session;
       if (session != null) {
         _redirecting = true;
+<<<<<<< HEAD
         
+=======
+
+>>>>>>> e8fc3d7 (fix: UI)
         Navigator.of(context).pushReplacementNamed('/home');
       }
     });
@@ -91,8 +99,10 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.pink[100],
+<<<<<<< HEAD
       appBar: AppBar(
         backgroundColor: Colors.pink[100],
         centerTitle: true,
@@ -168,6 +178,75 @@ class _LoginPageState extends State<LoginPage> {
             ),
           )
         ],
+=======
+      body: SafeArea(
+        minimum: EdgeInsets.all(size.width * 0.05),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Campus Cupid",
+                style: GoogleFonts.pacifico(
+                    fontSize: size.height * 0.08,
+                    color: const Color.fromARGB(221, 27, 27, 27)),
+              ),
+              SizedBox(height: size.height * 0.14),
+              Text(
+                "Sign Up",
+                style: GoogleFonts.pacifico(
+                    fontSize: size.height * 0.06,
+                    color: const Color.fromARGB(221, 27, 27, 27)),
+              ),
+              SizedBox(height: size.height * 0.04),
+              Icon(Icons.heart_broken_sharp, size: 100, color: Colors.red[900]),
+              SizedBox(height: size.height * 0.04),
+              Text(
+                'Sign in via the magic link with your email below',
+                style: TextStyle(fontSize: size.height * 0.02),
+              ),
+              Padding(
+                padding: EdgeInsets.all(size.width * 0.06),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(color: Colors.red[600]),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red, width: 2.0),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(16),
+                          )),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.red, width: 2.0),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                      )),
+                  style: TextStyle(color: Colors.red[600]),
+                ),
+              ),
+              SizedBox(height: size.height * 0.02),
+              ElevatedButton(
+                onPressed: _isLoading ? null : _signIn,
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.resolveWith(
+                    (states) => Colors.pink[100],
+                  ),
+                  backgroundColor: MaterialStateProperty.resolveWith(
+                      (states) => Colors.red[900]),
+                ),
+                child: Text(_isLoading ? 'Loading' : 'Send Magic Link'),
+              ),
+            ],
+          ),
+        ),
+>>>>>>> e8fc3d7 (fix: UI)
       ),
     );
   }
